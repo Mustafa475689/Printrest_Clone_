d4c2e6dad3f8a0df5594c21e9f930285ac9257ee
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-06-22 18:39:10 +0500

    multiple deployments
    multiple deployments

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 function connectDB() {
-
-  console.log("MONGO_URL =", process.env.MONGO_URL);
-
-  mongoose.connect(process.env.MONGO_URL)
+  return mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log("DB Error:", err);
+      process.exit(1);
+    });
 }
 
 module.exports = connectDB;
