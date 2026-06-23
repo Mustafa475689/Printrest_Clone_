64e5692e578a76ca11f1c6208f7ebde640d40c4b
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-06-23 15:36:51 +0500

    update config
    update config

--- config/multer.js
+++ config/multer.js
@@ -1,16 +1,20 @@
-const multer = require("multer");
-const path = require("path");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads");
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + path.extname(file.originalname);
-    cb(null, uniqueName);
-  }
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 });
 
-const upload = multer({ storage });
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'printrest_uploads', // Cloudinary mein folder ka naam
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
 
+const upload = multer({ storage: storage });
 module.exports = upload;
